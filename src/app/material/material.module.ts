f5ede4b1e34459ec4ba72bb4c120f878617f6be2
Adamari <adamari3939@gmail.com>
2021-11-06 10:38:23 -0400

    uuu

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
+  ]
+})
+
+export class MaterialModule { }
